57c79666a14dbb96effb098c4c073cedecb6caee
Jesse Engerman <jesseengerman@hotmail.com>
2020-02-29 14:40:06 -0600

    Sponza, objLoader download

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const OBJMTLLoader = require('obj-mtl-loader');
+const objMtlLoader = new OBJMTLLoader();
+
+const objPath = '../assets/sponza/sponza.obj';
+const mtlPath = '../assets/sponza/sponza.mtl';
+
+const canvas = document.querySelector('canvas');
+const gl = canvas.getContext('webgl2');
+
+objMtlLoader.load(objPath, mtlPath, function(error, result){
+    if(error) {
+        console.log(error);
+        return;
+    }
+
+    console.log(result);
+})
